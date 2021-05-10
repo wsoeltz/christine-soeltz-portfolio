1c152afb0f61be5350d6d5b39feb514fbc33be82
Kyle Soeltz <wsoeltz@gmail.com>
2021-05-10 14:44:08 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  siteMetadata: {
+    title: "Christine Soeltz' Portfolio",
+  },
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+  ],
+};
